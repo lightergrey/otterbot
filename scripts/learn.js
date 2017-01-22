5a02a9f31cbdda79772e4ae77b18cec1bfe1a269
lightergrey <hi@joshstewart.me>
2017-01-22 14:52:18 -0500

    Add better dump commands

--- scripts/learn.js
+++ scripts/learn.js
@@ -96,14 +96,44 @@ module.exports = (controller) => {
     });
   });
 
-  controller.hears(['dump'], 'direct_message,direct_mention,mention', (bot, message) => {
+  controller.hears(['^dump (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
+    const id = message.match[1];
+    controller.storage.teams.get(id, (err, data) => {
+      if (err) {
+        bot.reply(message, `Something went wrong: ${err}`);
+        return;
+      }
+
+      bot.api.files.upload({
+        content: `${JSON.stringify(data, null, 2)}`,
+        filename: `${id}.json`,
+        filetype: 'json',
+        channels: message.channel,
+      }, (err) => {
+        if (err) {
+          bot.reply(message, `Sorry, there has been an error: ${err}`);
+        }
+      });
+    });
+  });
+
+  controller.hears(['^dump'], 'direct_message,direct_mention,mention', (bot, message) => {
     controller.storage.teams.all((err, data) => {
       if (err) {
         bot.reply(message, `Something went wrong: ${err}`);
         return;
       }
 
-      bot.reply(message, `Dump:\n\`\`\`\n${JSON.stringify(data, null, 2)}\n\`\`\``);
+      bot.api.files.upload({
+        content: `${JSON.stringify(data.map(item => item.id), null, 2)}`,
+        filename: 'ids.json',
+        filetype: 'json',
+        channels: message.channel,
+      }, (err) => {
+        if (err) {
+          bot.reply(message, `Sorry, there has been an error: ${err}`);
+        }
+      });
     });
   });
 
