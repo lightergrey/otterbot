0cd0b1d8013d7f3ae4f37b4d46029cc3ca96fee7
lightergrey <hi@joshstewart.me>
2017-01-26 18:29:39 -0500

    Closes #7 Add pug me

---
+++ scripts/pug-me.js
@@ -0,0 +1,27 @@
+/**
+ * • `@bot pug me` Receive a pug
+ * • `@bot pug bomb <count>` Receive <count> pugs, default 5
+ */
+
+const got = require('got');
+
+module.exports = controller => {
+  controller.hears([/^pug me/i], 'direct_message,direct_mention,mention,ambient', (bot, message) => {
+    got('http://pugme.herokuapp.com/random')
+      .then(response => {
+        bot.reply(message, JSON.parse(response.body).pug);
+      }).catch(err => {
+        bot.reply(message, `\`oh no\` ${err}`);
+      });
+  });
+
+  controller.hears([/^pug bomb( (\d+))?/i], 'direct_message,direct_mention,mention,ambient', (bot, message) => {
+    const count = message.match[1] || 5;
+    got(`http://pugme.herokuapp.com/bomb?count=${count}`)
+      .then(response => {
+        bot.reply(message, JSON.parse(response.body).pugs.join('\n'));
+      }).catch(err => {
+        bot.reply(message, `\`oh no\` ${err}`);
+      });
+  });
+};
