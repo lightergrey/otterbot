051def2c01dd710fd99d7b611441cd78d65a044d
lightergrey <hi@joshstewart.me>
2017-01-26 10:50:44 -0500

    Closes #18 Forget works like learn

--- scripts/learn.js
+++ scripts/learn.js
@@ -9,12 +9,11 @@
  */
 
 module.exports = controller => {
-  controller.hears([/^learn (.*) \| (.*)/i], 'direct_message,direct_mention,mention', (bot, message) => {
-    const id = message.match[1];
-    const value = message.match[2];
+  controller.hears([/^learn ([\w-]+) \| (.*)$/i], 'direct_message,direct_mention,mention', (bot, message) => {
+    const [, id, value] = message.match;
 
     if (!id || !value) {
-      bot.reply(message, 'I didn’t get that. I need a <id> | <value>');
+      bot.reply(message, 'I didn’t get that. Try `learn <id> | <value>`');
       return;
     }
 
@@ -35,27 +34,53 @@ module.exports = controller => {
           return;
         }
 
-        bot.reply(message, `Learned ${id}: ${value}`);
+        bot.reply(message, `I learned "${id}": "${value}"`);
       });
     });
   });
 
-  controller.hears([/^forget (.*)/], 'direct_message,direct_mention,mention', (bot, message) => {
-    const id = message.match[1];
+  controller.hears([/^forget ([\w-]+)(?: \| (.*))?/], 'direct_message,direct_mention,mention', (bot, message) => {
+    const [, id, value] = message.match;
+    console.log(id, value);
 
     if (!id) {
-      bot.reply(message, 'I didn’t get that. I need an <id>');
+      bot.reply(message, 'I didn’t get that. Try `forget <id>` or `forget <id> | <value>`');
       return;
     }
 
-    controller.storage.teams.remove(id, err => {
-      if (err) {
-        bot.reply(message, `Something went wrong: ${err}`);
-        return;
-      }
+    if (!value) {
+      controller.storage.teams.remove(id, err => {
+        if (err) {
+          bot.reply(message, `Something went wrong: ${err}`);
+          return;
+        }
 
-      bot.reply(message, 'Forgot it');
-    });
+        bot.reply(message, `I forgot "${id}"`);
+      });
+    }
+
+    if (value) {
+      controller.storage.teams.get(id, (err, data) => {
+        if (err) {
+          bot.reply(message, `Something went wrong: ${err}`);
+          return;
+        }
+
+        const newData = {
+          id,
+          values: data.values.filter(storedValue => storedValue !== value)
+        };
+
+        controller.storage.teams.save(Object.assign({}, data, newData), saveErr => {
+          if (saveErr) {
+            bot.reply(message, `Something went wrong: ${err}`);
+            return;
+          }
+
+          bot.reply(message, `I forgot "${id}": "${value}"`);
+        });
+      });
+    }
   });
 
   controller.hears([/^list (.*)/], 'direct_message,direct_mention,mention', (bot, message) => {
@@ -73,11 +98,11 @@ module.exports = controller => {
       }
 
       if (!data) {
-        bot.reply(message, `Couldn’t find id:${id}`);
+        bot.reply(message, `I couldn’t find "${id}"`);
         return;
       }
 
-      bot.reply(message, JSON.stringify(data.values));
+      bot.reply(message, data.values.reduce((prev, curr, index) => prev + `${index + 1}. ${curr}\n`, ''));
     });
   });
 
@@ -96,34 +121,13 @@ module.exports = controller => {
       }
 
       if (!data) {
-        bot.reply(message, `Couldn’t find id:${id}`);
+        bot.reply(message, `Couldn’t find id: ${id}`);
         return;
       }
 
       const i = Math.floor(Math.random() * data.values.length);
 
       bot.reply(message, data.values[i]);
-    });
-  });
-
-  controller.hears(['^dump (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-    const id = message.match[1];
-    controller.storage.teams.get(id, (err, data) => {
-      if (err) {
-        bot.reply(message, `Something went wrong: ${err}`);
-        return;
-      }
-
-      bot.api.files.upload({
-        content: `${JSON.stringify(data, null, 2)}`,
-        filename: `${id}.json`,
-        filetype: 'json',
-        channels: message.channel
-      }, err => {
-        if (err) {
-          bot.reply(message, `Sorry, there has been an error: ${err}`);
-        }
-      });
     });
   });
 
