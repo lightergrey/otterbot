e7eaaa82ae91d16fb6166b28f5efa9e61600be53
lightergrey <hi@joshstewart.me>
2017-02-02 15:47:36 -0500

    oh no

---
+++ skills/oh-no.js
@@ -0,0 +1,9 @@
+/**
+ * • `oh no` oh no
+ */
+
+module.exports = controller => {
+  controller.hears(['`oh no`'], 'direct_message,direct_mention,mention,ambient', (bot, message) => {
+    bot.reply(message, '`oh no`');
+  });
+};
