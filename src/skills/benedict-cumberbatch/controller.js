00a9b28eb9db556ec9feeaefc51cabbe7c2f183d
Josh Stewart <jstewart@etsy.com>
2019-01-19 07:05:05 -0500

    Use async await for cumberbatch

--- src/skills/benedict-cumberbatch/controller.js
+++ src/skills/benedict-cumberbatch/controller.js
@@ -3,31 +3,25 @@
  */
 const replyHandler = require("./replyHandler");
 const getReply = require("../../getReply");
+const getDataFromStorage = require("../../getDataFromStorage");
 
 module.exports = controller => {
   controller.hears(
     ["benedict cumberbatch"],
     "direct_message,direct_mention,mention,ambient",
-    (bot, message) => {
+    async (bot, message) => {
       const reply = getReply(bot, message);
-      controller.storage.teams.get("benedicts", (err, benedictsData) => {
-        if (err) {
-          reply(`Error getting benedicts: ${err}`);
-          return;
-        }
 
-        controller.storage.teams.get(
-          "cumberbatches",
-          (err, cumberbatchesData) => {
-            if (err) {
-              reply(`Error getting cumberbatches: ${err}`);
-              return;
-            }
-
-            replyHandler(reply, benedictsData, cumberbatchesData);
-          }
+      try {
+        const benedictsData = await getDataFromStorage(controller, "benedicts");
+        const cumberbatchesData = await getDataFromStorage(
+          controller,
+          "cumberbatches"
         );
-      });
+        replyHandler(reply, benedictsData, cumberbatchesData);
+      } catch (err) {
+        reply(`Error getting benedicts or cumberbatches: ${err}`);
+      }
     }
   );
 };
